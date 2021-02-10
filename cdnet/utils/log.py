3c5c452cc8bc2b31a5dc7c65c3921f1163dcf7e0
Duke <d@d-l.io>
2021-02-10 21:41:51 +0800

    utils/log: use local time as timestamp

--- cdnet/utils/log.py
+++ cdnet/utils/log.py
@@ -12,6 +12,6 @@ import logging
 logging.VERBOSE = 5
 
 def logger_init(level=logging.INFO):
-    logging.basicConfig(format='%(created).3f %(name)s: %(levelname)s: %(message)s', level=level)
+    logging.basicConfig(format='%(asctime)s.%(msecs)03d %(name)s: %(levelname)s: %(message)s', datefmt='%H:%M:%S', level=level)
     logging.addLevelName(logging.VERBOSE, 'VERBOSE')
 
